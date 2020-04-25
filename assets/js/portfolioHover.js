a9b109564ad8f6492215fa7b687b203cdfa5b2d8
Tanner Griffin <tannermgriffin@gmail.com>
2020-04-24 21:31:33 -0500

    added hover javascript for portfolio items, added different images for portfolio items, reorganized folder structure adding assets folder & fixed ref links accordingly

---
+++ assets/js/portfolioHover.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+    $(".item1").hover(function(){
+      $("#img1").css("filter", "opacity(85%)");
+      }, function(){
+      $("#img1").css("filter", "opacity(60%) blur(4px)");
+    });
+
+    $(".item2").hover(function(){
+      $("#img2").css("filter", "opacity(85%)");
+      }, function(){
+      $("#img2").css("filter", "opacity(60%) blur(4px)");
+    });
+
+    $(".item3").hover(function(){
+      $("#img3").css("filter", "opacity(85%)");
+      }, function(){
+      $("#img3").css("filter", "opacity(60%) blur(4px)");
+    });
+
+    $(".item4").hover(function(){
+      $("#img4").css("filter", "opacity(85%)");
+      }, function(){
+      $("#img4").css("filter", "opacity(60%) blur(4px)");
+    });
+
+    $(".item5").hover(function(){
+      $("#img5").css("filter", "opacity(85%)");
+      }, function(){
+      $("#img5").css("filter", "opacity(60%) blur(4px)");
+    });
+
+    $(".item6").hover(function(){
+      $("#img6").css("filter", "opacity(85%)");
+      }, function(){
+      $("#img6").css("filter", "opacity(60%) blur(4px)");
+    });
+  
+  });
