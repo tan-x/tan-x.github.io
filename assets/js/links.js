e561b2a38742292e7892072d4c613b6c13063813
Tanner Griffin <tannermgriffin@gmail.com>
2020-06-22 15:35:52 -0500

    Created modal template with image and github/site buttons to link

--- assets/js/links.js
+++ assets/js/links.js
@@ -1,23 +1,24 @@
 $('.port-item').on('click', (e) => {
     console.log(e.target.parentElement.id);
-    switch (e.target.parentElement.id) {
-        case 'item1':
-            window.open('https://github.com/pubIO-2020/pubIO', '_blank');
-            break;
-        case 'item2':
-            window.open('https://tan-x.github.io/weather_dashboard/', '_blank');
-            break;
-        case 'item3':
-            window.open('https://tan-x.github.io/code_quiz/', '_blank');
-            break;
-        case 'item4':
-            window.open('https://github.com/tan-x/organize-mobilize', '_blank');
-            break;
-        case 'item5':
-            window.open('https://tan-x.github.io/drum_kit/', '_blank');
-            break;
-        case 'item6':
-            window.open('https://tan-x.github.io/work_day_scheduler/', '_blank');
-            break;
-    }
+    $("#myModal").modal();
+    // switch (e.target.parentElement.id) {
+    //     case 'item1':
+    //         window.open('https://github.com/pubIO-2020/pubIO', '_blank');
+    //         break;
+    //     case 'item2':
+    //         window.open('https://tan-x.github.io/weather_dashboard/', '_blank');
+    //         break;
+    //     case 'item3':
+    //         window.open('https://tan-x.github.io/code_quiz/', '_blank');
+    //         break;
+    //     case 'item4':
+    //         window.open('https://github.com/tan-x/organize-mobilize', '_blank');
+    //         break;
+    //     case 'item5':
+    //         window.open('https://tan-x.github.io/drum_kit/', '_blank');
+    //         break;
+    //     case 'item6':
+    //         window.open('https://tan-x.github.io/work_day_scheduler/', '_blank');
+    //         break;
+    // }
 })
